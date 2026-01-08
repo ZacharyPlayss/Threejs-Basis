53c7cece0ecf6596ecd6742ac862642f2c7653b5
Zachary Van De Staey <zachary.vandestaey@student.kdg.be>
2026-01-08 20:09:08 +0100

    Feat: initiated testing suite for automated testing purposes.

---
+++ automated_testing/vitest.config.ts
@@ -0,0 +1,19 @@
+import {defineConfig} from 'vitest/config';
+import path from 'path';
+
+export default defineConfig({
+    server: {
+        fs: {
+            allow: ['..']
+        }
+    },
+    test: {
+        globals: true,
+
+        environment: 'jsdom',
+
+        alias: {
+            '@scripts': path.resolve(__dirname, '../scripts')
+        },
+    },
+});
